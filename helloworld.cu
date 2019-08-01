a030dff14a3d36bac6dc2187a71e8f99158d3576
Benjamin Saljooghi <benjamin.saljooghi@gmail.com>
2019-08-01 13:37:51 +1000

    Compiles but intellisense doesn't work

---
+++ helloworld.cu
@@ -0,0 +1,55 @@
+﻿#include "cuda.h"
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+
+#ifdef __CUDACC__
+#define CUDA_CALLABLE_MEMBER __host__ __device__
+#define KERNEL_ARGS2(grid, block) <<< grid, block >>>
+#define KERNEL_ARGS3(grid, block, sh_mem) <<< grid, block, sh_mem >>>
+#define KERNEL_ARGS4(grid, block, sh_mem, stream) <<< grid, block, sh_mem, stream >>>
+#else
+#define CUDA_CALLABLE_MEMBER
+#define KERNEL_ARGS2(grid, block)
+#define KERNEL_ARGS3(grid, block, sh_mem)
+#define KERNEL_ARGS4(grid, block, sh_mem, stream)
+#endif
+
+#include <stdio.h>
+
+const int N = 16;
+const int blocksize = 16;
+
+__global__ void hello(char* a, int* b)
+{
+	a[threadIdx.x] += b[threadIdx.x];
+}
+
+int main()
+{
+	char a[N] = "Hello \0\0\0\0\0\0";
+	int b[N] = { 15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	char* ad;
+	int* bd;
+	const int csize = N * sizeof(char);
+	const int isize = N * sizeof(int);
+
+	printf("%s", a);
+
+	cudaMalloc((void**)& ad, csize);
+	cudaMalloc((void**)& bd, isize);
+	cudaMemcpy(ad, a, csize, cudaMemcpyHostToDevice);
+	cudaMemcpy(bd, b, isize, cudaMemcpyHostToDevice);
+
+	dim3 dimBlock(blocksize, 1);
+	dim3 dimGrid(1, 1);
+	hello KERNEL_ARGS2(dimGrid, dimBlock) (ad, bd);
+	
+	cudaMemcpy(a, ad, csize, cudaMemcpyDeviceToHost);
+	cudaFree(ad);
+	cudaFree(bd);
+
+	printf("%s\n", a);
+	return EXIT_SUCCESS;
+}
